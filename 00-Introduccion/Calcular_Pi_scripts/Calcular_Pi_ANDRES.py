c238109eb7d35ce0fe7eeea304b7cfdfca8b84fc
drodriguez32 <d.rodriguez32@uniandes.edu.co>
2018-02-05 08:25:43 -0500

    Interpolation schemes
    Clase del 31 de enero donde vimos interpolaciones

--- 00-Introduccion/Calcular_Pi_scripts/Calcular_Pi_ANDRES.py
+++ 00-Introduccion/Calcular_Pi_scripts/Calcular_Pi_ANDRES.py
@@ -8,22 +8,22 @@ def coord_aleat(N_coord):
 
 def pi_calc(N,r):
     a = r #radius
-    x, y = coord_aleat(N)
-    pos = [(x**2 + y**2 - a**2) <= 0]
-    pts_i = np.sum(pos)
-    PN = (pts_i/N)
-    error = PN - (np.pi/4)
+    x, y = coord_aleat(N)  # llamar la función para crear coordinadas aleatorias
+    pos = [(x**2 + y**2 - a**2) <= 0]  # puntos que están dentro del círculo
+    pts_i = np.sum(pos) # sumar la cantidad de puntos
+    PN = (pts_i/N)  # probabilidad de que los puntos estén adentro
+    error = PN - (np.pi/4)  # el error de la probabilidad calculada
     return error
 
-N_datos = np.logspace(1,7,500)
-PN_a = []
-radius = 1.0
+N_datos = np.logspace(1,6,500)  # creo una lista con la cantidad de datos que voy a tomar
+PN_a = []  # arreglo vacío para guardar los datos de error
+radius = 1.0  # radio del círculo
 
 for i in N_datos:
-    err = pi_calc(int(i),radius)
-    PN_a.append(err)
+    err = pi_calc(int(i),radius)  # llamar la función para calcular pi y guardar el valor del error
+    PN_a.append(err) # agregar el error al arreglo
     
 plt.semilogx(N_datos,PN_a)
 plt.grid(True) 
 plt.xlabel("N - número de puntos")
-plt.ylabel("P(N) - pi/4")
+plt.ylabel(" $P(N) - \pi/4$ ")
